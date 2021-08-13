9f5c87f8d218ac989a5cef7b7b8ae5a3d206f6bb
anay333 <87089288+anay333@users.noreply.github.com>
2021-08-14 02:03:40 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket;
+        ticket = 5019;
+        int onebonusMile;
+        onebonusMile = 20;
+        int totalbonus = ticket / onebonusMile;
+        System.out.println(totalbonus);
+    }
+}
